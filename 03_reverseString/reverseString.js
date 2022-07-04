feb9052c81ebcf0769a9b449b14f3d5b497fcee4
kwojtkowiak <80783985+kwojtkowiak@users.noreply.github.com>
2022-07-04 15:25:47 +0200

    Test task no 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,8 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+let splitString = str.split("")
+let reversedStr = splitString.reverse();
+let joinArray = reversedStr.join("")
+return joinArray;
 };
 
 // Do not edit below this line
