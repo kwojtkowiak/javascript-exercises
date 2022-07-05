c612f4ffa6fa19c84df547dcd26235c007cdeab4
kwojtkowiak <80783985+kwojtkowiak@users.noreply.github.com>
2022-07-05 12:16:52 +0200

    Solve sumAll

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,14 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...objectToRemove) {
+    let i = 0
+    while (i < array.length) {
+      if (array[i] === objectToRemove.includes(array[i])) {
+        array.splice(i,1);
+    }
+      else {
+        i++;
+      }
+    }
+    return array
 };
 
 // Do not edit below this line
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
-
+const sumAll = function(Num1,Num2) {
+    if (!Number.isInteger(Num1) || !Number.isInteger(Num2)) return "ERROR";
+    if (Num1 < 0 || Num2 < 0) return "ERROR";
+    if (Num1 > Num2) {
+        const temp = Num1;
+        Num1 = Num2;
+        Num2 = temp;
+      }
+    
+    let finalSum = 0;
+    for (let i = Num1; i <= Num2; i++) {
+        finalSum += i;
+    }
+    return finalSum
 };
 
 // Do not edit below this line
