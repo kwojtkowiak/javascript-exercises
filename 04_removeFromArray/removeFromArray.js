eb445d895bc105f5122fda0bd4df17672382e0b1
kwojtkowiak <80783985+kwojtkowiak@users.noreply.github.com>
2022-07-05 12:52:26 +0200

    Leap years fix (is not divisible by 100)

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,14 +1,13 @@
-const removeFromArray = function(array, ...objectToRemove) {
-    let i = 0
-    while (i < array.length) {
-      if (array[i] === objectToRemove.includes(array[i])) {
-        array.splice(i,1);
-    }
-      else {
-        i++;
+const removeFromArray = function(...args) {
+    const array = args[0];
+    const newArray = [];
+
+    array.forEach((item) => {
+      if (!args.includes(item)) {
+        newArray.push(item);
       }
-    }
-    return array
+    })
+    return newArray;
 };
 
 // Do not edit below this line
--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,4 +1,5 @@
-const leapYears = function() {
+const leapYears = function(leapYear) {
+    return year % 4 === 0 && ( year % 100 !== 0 || year % 400 === 0);
 
 };
 
