57bc3239cb96d7fe8eae0cd53e4d93e9e10c8a35
kwojtkowiak <80783985+kwojtkowiak@users.noreply.github.com>
2022-07-05 13:09:28 +0200

    Fix temp conversion to display float nums

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,19 @@
-const ftoc = function() {
+const ftoc = function(fTemp) {
+  newCTemp = (fTemp - 32)/1.8;
+  if (newCTemp !== 0) {
+    return parseFloat(newCTemp.toFixed(1));
+  } else 
+  {return parseFloat(newCTemp);
+  }
+}
 
-};
-
-const ctof = function() {
-
+const ctof = function(cTemp) {
+  newFTemp = cTemp*1.8+32;
+  if (newFTemp !== 0) {
+    return parseFloat(newFTemp.toFixed(1));
+  } else 
+  {return parseFloat(newFTemp);
+  }
 };
 
 // Do not edit below this line
